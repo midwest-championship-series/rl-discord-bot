42520a2b4c49efa636fa5edb9a29c0fd730d942d
corbfon <corbin.fonville@gmail.com>
2019-02-23 14:14:03 -0800

    add env variables to control protocol and hostname

--- src/api/v1/auth/discord.ts
+++ src/api/v1/auth/discord.ts
@@ -16,11 +16,10 @@ const credentials: ModuleOptions = {
 
 // Initialize the OAuth2 Library
 const oauth2 = create(credentials)
-const redirect_uri = 'http://localhost:25257/api/v1/auth/discord/callback'
+const redirect_uri = `${process.env.PROTOCOL}://${process.env.HOSTNAME}/api/v1/auth/discord/callback`
 const scope = ['connections']
 
 export function DiscordRedirect(req: Request, res: Response, next: NextFunction): void {
-    console.log('in the redirect route')
     // Authorization oauth2 URI
     const authorizationUri = oauth2.authorizationCode.authorizeURL({
         redirect_uri,
