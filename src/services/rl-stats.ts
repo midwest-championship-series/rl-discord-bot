8d86a2b75c629a2bc7a4ab1da3712b53c98ee192
corbfon <corbin.fonville@gmail.com>
2020-03-28 21:44:57 -0500

    add replays with put

--- src/services/rl-stats.ts
+++ src/services/rl-stats.ts
@@ -23,9 +23,9 @@ const put = (table: string, body: any) => {
 
 const report = (gameIds, reporterDiscordId) => {
   return request({
-    method: 'POST',
+    method: 'PUT',
     url: [baseUrl, 'games', '_report'].join('/'),
-    body: { game_ids: gameIds, reporter_discord_id: reporterDiscordId },
+    body: { game_ids: gameIds },
   })
 }
 
