681db38c8ef71c91f3a680c95d9e65a4f81bcba2
corbfon <corbin.fonville@gmail.com>
2020-05-16 12:04:35 -0500

    stop checking if reporting player is from mncs

--- src/services/discord/bot/report.ts
+++ src/services/discord/bot/report.ts
@@ -21,24 +21,17 @@ const report = async msg => {
           .slice(-1)[0],
     )
   // find out if user has a linked account
-  const members = await rlStats.get('members', { discord_id: msg.author.id })
-  if (members && members.length > 0) {
-    try {
-      // report scores
-      const { recorded_ids } = await rlStats.report({ gameIds })
-      msg.channel.send(`Thank you for the report, @${msg.author.username}!\nGames reported: ${recorded_ids.join(', ')}`)
-    } catch (err) {
-      console.error(err)
-      if (err.body && err.body.error) {
-        throw new Error(err.body.error)
-      } else {
-        throw err
-      }
+  try {
+    // report scores
+    const { recorded_ids } = await rlStats.report({ gameIds })
+    msg.channel.send(`Thank you for the report, @${msg.author.username}!\nGames reported: ${recorded_ids.join(', ')}`)
+  } catch (err) {
+    console.error(err)
+    if (err.body && err.body.error) {
+      throw new Error(err.body.error)
+    } else {
+      throw err
     }
-  } else {
-    // ask user to link account and re-report
-    msg.channel.send(`Unknown MNRL player - please link your account. I'll send instructions in a DM.`)
-    requestLinkAccount(msg.author)
   }
 }
 
