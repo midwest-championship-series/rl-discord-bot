98473f7c1b97a2e8de1f691c38c718a4c9a3463e
corbfon <corbin.fonville@gmail.com>
2020-05-23 10:32:02 -0500

    update link command

--- src/services/discord/bot/dm.ts
+++ src/services/discord/bot/dm.ts
@@ -4,8 +4,7 @@ export const requestLinkAccount = author => {
   let dm = ''
   dm += `In order to report games, you have to link your steam or xbox account to Discord, then go here: ${accountLinkURL} to link your Discord to MNRL.\n`
   dm += `You will be redirected to discordapp.com, which is Discord's website for authentication, to link your account to MNRL.\n`
-  dm += `If you were trying to report scores, please re-report them for MNCS stats processing.\n`
-  dm += `If you have any concerns, please reach out to Tero or the MNCS stats team in the MNRL or MNCS discords.`
+  dm += `If you have any concerns, please reach out to Tero or the MNCS stats team in discord.`
   author.send(dm)
 }
 
