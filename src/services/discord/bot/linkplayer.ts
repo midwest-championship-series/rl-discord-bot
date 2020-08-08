e7fa685832b2874ca6d57ec5e3a38af6c784215d
corbfon <corbin.fonville@gmail.com>
2020-08-08 16:31:30 -0500

    add linkplayer command

---
+++ src/services/discord/bot/linkplayer.ts
@@ -0,0 +1,32 @@
+import rlStats from '../../../services/rl-stats'
+
+const linkPlayer = async msg => {
+  const platforms = ['xbox', 'ps4', 'steam']
+  const user = msg.mentions.users.values().next().value
+  console.log('user', user)
+  const { id, username, avatar } = user
+  const linkAccounts = msg.content
+    .split(' ')
+    .filter(str => platforms.some(p => str.includes(p)))
+    .map(acc => acc.split(':'))
+    .map(acc => ({ platform: acc[0], platform_id: acc[1] }))
+
+  const [dbUser] = await rlStats.get('players', { discord_id: id })
+  if (!dbUser) throw new Error(`no player exists for: ${username}. Please link the player to a team`)
+
+  const accounts = dbUser.accounts.concat(
+    linkAccounts.filter(
+      acc => !dbUser.accounts.some(dbAcc => dbAcc.platform === acc.platform && dbAcc.platform_id === acc.platform_id),
+    ),
+  )
+
+  const update = {
+    screen_name: username,
+    avatar: `https://cdn.discordapp.com/avatars/${id}/${avatar}.png`,
+    accounts,
+  }
+  const updatedUser = await rlStats.put(`players/${dbUser._id}`, update)
+  msg.channel.send(`successfully updated user: ${updatedUser.screen_name}`)
+}
+
+export default linkPlayer
