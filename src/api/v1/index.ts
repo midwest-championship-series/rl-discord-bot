7ac3204a1e00f8dd8f9dc3b7a185383280c6cbe5
corbfon <corbin.fonville@gmail.com>
2020-08-10 17:59:30 -0500

    remove errors route in favor of more generic channels route

--- src/api/v1/index.ts
+++ src/api/v1/index.ts
@@ -1,13 +1,11 @@
 import { Router } from 'express'
 
 import AuthRouter from './auth'
-import ErrorsRouter from './errors'
 import ChannelsRouter from './channels'
 
 const router = Router()
 
 router.use('/auth', AuthRouter)
-router.use('/errors', ErrorsRouter)
 router.use('/channels', ChannelsRouter)
 
 export default router
