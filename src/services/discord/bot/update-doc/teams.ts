85559bc3b223baed19a69e1f11597fcd1cd23493
Benjamin Nylund <31191383+bnylund@users.noreply.github.com>
2021-05-17 17:52:50 -0500

    added vars update rule

--- src/services/discord/bot/update-doc/teams.ts
+++ src/services/discord/bot/update-doc/teams.ts
@@ -4,6 +4,17 @@ const update: UpdateMap = {
   name: overwrite,
   avatar: overwrite,
   hex_color: overwrite,
+  vars: data => {
+    const [property, value] = data.value.split(',')
+    const vars = data.doc.vars
+    const varToUpdate = vars.find(v => v.key === property)
+    if(value === 'undefined') {
+      delete varToUpdate[property]
+    } else {
+      varToUpdate[property] = value
+    }
+    return { vars }
+  },
 }
 
 export default update
