9f549c8e87883beaaa5af0c602493efc6c57f80f
corbfon <corbin.fonville@gmail.com>
2021-07-29 16:59:12 -0500

    add discord_id to possible team updates

--- src/services/discord/bot/update-doc/teams.ts
+++ src/services/discord/bot/update-doc/teams.ts
@@ -4,11 +4,12 @@ const update: UpdateMap = {
   name: overwrite,
   avatar: overwrite,
   hex_color: overwrite,
+  discord_id: overwrite,
   vars: data => {
     const [property, value] = data.value.split(',')
     const vars = data.doc.vars.filter(x => x.key !== property)
-    if(value !== 'undefined') {
-      vars.push({key: property, value: value})
+    if (value !== 'undefined') {
+      vars.push({ key: property, value: value })
     }
     return { vars }
   },
