8ccaf82c61bf408dd7170c742fb7ac01ec5b06d5
corbfon <corbin.fonville@gmail.com>
2023-04-04 15:23:58 -0500

    fix embed posting

--- src/api/v1/channels/index.ts
+++ src/api/v1/channels/index.ts
@@ -5,21 +5,25 @@ import { bot } from '../../../services/discord/bot'
 const router = Router()
 
 router.post('/:channelId', async (req, res, next) => {
-  const { channelId } = req.params
-  console.log('sending message to channel', channelId)
-  const channel: any = await bot.channels.fetch(channelId)
-  let { message, embed } = req.body
-  if (message) {
-    if (message.length > 2000) {
-      message = message.substring(0, 1999)
+  try {
+    const { channelId } = req.params
+    console.log('sending message to channel', channelId)
+    const channel: any = await bot.channels.fetch(channelId)
+    let { message, embed } = req.body
+    if (message) {
+      if (message.length > 2000) {
+        message = message.substring(0, 1999)
+      }
+      channel.send(message)
     }
-    channel.send(message)
+    if (embed) {
+      embed.timestamp = new Date()
+      channel.send({ embeds: [embed] })
+    }
+    res.status(200).send({ success: true })
+  } catch (err) {
+    next(err)
   }
-  if (embed) {
-    embed.timestamp = new Date()
-    channel.send({ embed })
-  }
-  res.status(200).send({ success: true })
 })
 
 export default router
