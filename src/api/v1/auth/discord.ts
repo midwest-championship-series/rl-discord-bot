0fa809436e61c155bafbc048e851606739c0b06a
Corbin Fonville <corbin.fonville@gmail.com>
2023-10-05 18:08:14 -0500

    fix screen names

--- src/api/v1/auth/discord.ts
+++ src/api/v1/auth/discord.ts
@@ -29,6 +29,7 @@ const syncPlayers = async discordUser => {
 
   if (!players[0]) {
     return rlStats.post('players', {
+      screen_name: discordUser.username,
       discord_id: discordUser.id,
       accounts,
       email: discordUser.email,
